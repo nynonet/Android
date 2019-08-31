ee66f9c562c1ebc06309743b5510d533c3c39563
Andeson Jesus de Menezes <nynonet@gmail.com>
2019-08-30 22:18:43 -0300

    Aula do dia 30/08/2019

--- Gti2019/app/src/main/java/edu/fasb/aulagti2019/Main2Activity.java
+++ Gti2019/app/src/main/java/edu/fasb/aulagti2019/Main2Activity.java
@@ -10,18 +10,26 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.fasb.aulagti2019.lista_person.AdapterCurso;
+import edu.fasb.aulagti2019.lista_person.Curso;
+
 public class Main2Activity extends AppCompatActivity {
 
-    private Button btn1;
-    private ListView list1;
+    private Button btn1;        //botão voltar do layout
+    private ListView list1;     //listview de cursos
+    private List<Curso> cursos; //Lista de cursos
+    private AdapterCurso adapterCurso;  //Adapater do Curso p/nossa lista view personalizada
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        super.onCreate(savedInstanceState);  //cria a instância
+        setContentView(R.layout.activity_main2);  //carrega o layout da Activity
 
-        btn1 = (Button) findViewById(R.id.btnTela2);
-        list1 = (ListView) findViewById(R.id.listview_gti);
+        btn1 = (Button) findViewById(R.id.btnTela2);    //vincula o botão
+        list1 = (ListView) findViewById(R.id.listview_gti); //vincula o listview
 
         //programando o botão
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -45,10 +53,32 @@ public class Main2Activity extends AppCompatActivity {
                 telaResult.putExtra("Nome", nome);
 
                 startActivity( telaResult );
-
-
             }
         });
 
+        //chamar a função p/carregar os dados na tela
+        CarregaDados();
+
     }
+
+    private void CarregaDados() {
+        this.cursos = new ArrayList<>(); //cria a lista de cursos
+
+        //Cria um curso dentro da lista
+        this.cursos.add( new Curso(1, "Programação",
+                R.drawable.ic_programa) );
+
+        this.cursos.add( new Curso(2, "Acorda p/Vida",
+                R.drawable.ic_acorda) );
+
+        this.cursos.add( new Curso(3, "Sexta Feira - Recarregar",
+                R.drawable.ic_battery) );
+
+        //Cria o adapter personalizado com a lista de cursos criada
+        adapterCurso = new AdapterCurso(this, cursos);
+
+        //informa a listview quais são os dados e como deve apresenta-lós.
+        list1.setAdapter( adapterCurso );
+    }
+
 }
--- Gti2019/app/src/main/java/edu/fasb/aulagti2019/lista_person/AdapterCurso.java
+++ Gti2019/app/src/main/java/edu/fasb/aulagti2019/lista_person/AdapterCurso.java
@@ -1,32 +1,83 @@
 package edu.fasb.aulagti2019.lista_person;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
+import edu.fasb.aulagti2019.R;
+
+/**
+ * classe de apoio a personalização de uma ListView
+ */
 public class AdapterCurso extends BaseAdapter {
 
-    private List<Curso> lista;
+    private List<Curso> lista;      //cria minha lista de cursos
+    private LayoutInflater inflater; // criar um layout
+
+    /**
+     * Método construtor que recebe um contexto e uma lista de custo.
+     * @param context
+     * @param lista
+     */
+    public AdapterCurso(Context context, List<Curso> lista) {
+        this.lista = lista; //recebe a lista de curso
+        this.inflater = LayoutInflater.from(context); //cria um layout para o contexto
+    }
 
     @Override
     public int getCount() {
-        return lista.size();
+        return lista.size(); //retona o tamanho da lista
     }
 
     @Override
     public Object getItem(int position) {
-        return lista.get(position);
+        return lista.get(position); //retona o objeto selecionado da lista
     }
 
     @Override
     public long getItemId(int position) {
-        return lista.get(position).getId();
+        return lista.get(position).getId(); //retorna o ID do objeto selecionado
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    public View getView(int position, View view, ViewGroup parent) {
+        Auxiliar obj; //declaramos um objeto auxiliar
+
+        if ( view == null) {    //verificamos se já existe um layout carregado
+            //cria o layoute na variavel View do Item.
+            view = inflater.inflate(R.layout.minhalistview, null);
+            obj = new Auxiliar(); //criou o objeto auxiliar
+
+            //vincula a imagemview auxiliar ao do layout personalizado
+            obj.icone = (ImageView) view.findViewById(R.id.iconeLista);
+
+            //vincula a textview auxiliar ao do layout personalizado
+            obj.texto = (TextView) view.findViewById(R.id.textoLista);
+
+        } else {    //se já existe criado só vincula os dados ao objeto auxiliar
+            obj = (Auxiliar) view.getTag(); //atribui o objeto vindo do ListView
+        }
+
+        //passando o valor da lista p/o objeto visual
+        obj.texto.setText( lista.get(position).getTexto() );
+        obj.icone.setImageResource( lista.get(position).getIcone() );
+
+        //retorna a view que fora personalizada.
+        return view;
+    }
+
+    /**
+     * cria uma classe só com os objetos que estão em nosso layout
+     * personalizado para auxiliar na atribuição de valores.
+     */
+    private class Auxiliar {
+        ImageView icone;
+        TextView texto;
     }
 }
